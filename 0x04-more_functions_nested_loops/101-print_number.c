749feb849c61938327332a7abf5084246f71407e
Garkida <josephhassangk@gmail.com>
2023-08-21 04:21:13 -0700

    More of Functions

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - Prints a number
+ * @n: The number to print
+ */
+
+void print_number(int n)
+{
+	char ld, cr; 
+	int r; 
+	int ct = 0; 
+
+	if  (n < 0)
+	{
+		_putchar ('-'); 
+		;
+
+
+
